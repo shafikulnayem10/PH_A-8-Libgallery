fbfe0dbf0ff742166d5b372a5a996e38649b608c
shafikulnayem10 <inayem98@gmail.com>
2026-05-01 03:52:33 +0600

    added better auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { Button } from "@heroui/react";
 import Image from "next/image";
 
-export default function Home() {
+export default function Homepage() {
   return (
     <Button variant="primary" size="large">
       Hello World
