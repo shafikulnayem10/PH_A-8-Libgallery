077f5e2483b6d3c16749943dc579ec29f619f6df
shafikulnayem10 <inayem98@gmail.com>
2026-05-31 12:13:53 +0600

    added next dynamic for improve performance

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,26 @@
 export const dynamic = 'force-dynamic';
+
 import Banner from "@/components/Banner";
 import FeaturedBooks from "@/components/FeaturedBooks";
-import QuoteSection from "@/components/QuoteSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
-import { Button } from "@heroui/react";
-import Image from "next/image";
+import nextDynamic from "next/dynamic";
+
+const TestimonialsSection = nextDynamic(() => import("@/components/TestimonialsSection"), {
+  ssr: false,
+  loading: () => <div className="h-64" />,
+});
+
+const QuoteSection = nextDynamic(() => import("@/components/QuoteSection"), {
+  ssr: false,
+  loading: () => <div className="h-32" />,
+});
 
 export default function Homepage() {
   return (
-  <>
-    <Banner></Banner>
-    <FeaturedBooks></FeaturedBooks>
-    <TestimonialsSection></TestimonialsSection>
-    <QuoteSection></QuoteSection>
-  </>
+    <>
+      <Banner />
+      <FeaturedBooks />
+      <TestimonialsSection />
+      <QuoteSection />
+    </>
   );
 }
