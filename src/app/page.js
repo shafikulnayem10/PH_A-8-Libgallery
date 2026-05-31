1575c810ddbb3e4e87372485996c90640623ea89
shafikulnayem10 <inayem98@gmail.com>
2026-05-31 12:16:50 +0600

    added fetchcache

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-export const dynamic = 'force-dynamic';
-
+import dynamic from "next/dynamic";
 import Banner from "@/components/Banner";
 import FeaturedBooks from "@/components/FeaturedBooks";
-import nextDynamic from "next/dynamic";
 
-const TestimonialsSection = nextDynamic(() => import("@/components/TestimonialsSection"), {
+export const fetchCache = 'force-no-store'; 
+
+const TestimonialsSection = dynamic(() => import("@/components/TestimonialsSection"), {
   ssr: false,
   loading: () => <div className="h-64" />,
 });
 
-const QuoteSection = nextDynamic(() => import("@/components/QuoteSection"), {
+const QuoteSection = dynamic(() => import("@/components/QuoteSection"), {
   ssr: false,
   loading: () => <div className="h-32" />,
 });
